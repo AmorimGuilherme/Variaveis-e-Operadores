4600dccacd7f08713ba8dd7c455f062befd71dc0
Guilherme Amorim <guilhermedeamorim098@gmail.com>
2023-02-15 21:25:54 -0300

    Unificando Pasta de Exercicios Vetores e Matrizes na Main

--- src/vetoresEMatrizes/Main.java
+++ src/vetoresEMatrizes/Main.java
@@ -1,5 +1,6 @@
-package vetores;
+package vetoresEMatrizes;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -60,6 +61,34 @@ public class Main {
 		System.out.println();
 		System.out.println("Números pares: " + mostraPar);
 		System.out.println("Números ímpares: " + mostraImpar);
+		System.out.println();
+		System.out.println();
+
+		// Exercicio 04
+
+		int aluno = 10;
+		int bimestre = 4;
+		double mediaNotaAluno = 0.0;
+		double somaNotaAluno = 0.0;
+		double[] mediasAlunos = new double[aluno];
+		double[][] notasAlunosAnual = new double[aluno][bimestre];
+
+		for (int i = 0; i < notasAlunosAnual.length; i++) {
+			System.out.println();
+			somaNotaAluno = 0;
+			for (int j = 0; j < notasAlunosAnual[bimestre].length; j++) {
+				System.out.print("Informe a " + (j + 1) + "ª nota do " + (i + 1) + "º alune: ");
+				double notaAluno = scan.nextDouble();
+				somaNotaAluno += notaAluno;
+				mediaNotaAluno = somaNotaAluno / bimestre;
+				mediasAlunos[i] = mediaNotaAluno;
+
+			}
+		}
+
+		// for(double[] mediaTurma: notasAlunosAnual) {
+
+		System.out.println("Média dos alunes: " + Arrays.toString(mediasAlunos));
 
 		scan.close();
 
